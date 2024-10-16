c6ad2594b5781da99fa25fb40d0c8bb250b8a78f
Rouaa Ben ayed <112416975+Rouaaba@users.noreply.github.com>
2024-10-16 17:19:29 +0100

    Delete Downloads directory

--- Downloads/messaging/src/main/java/com/app/messaging/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.app.messaging.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.app.messaging.domain.User;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-
-}
